744acdda2c8f74cc40417ed28364361411b7be12
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:57:06 +0530

    Create printtype3.py

---
+++ printtype3.py
@@ -0,0 +1,12 @@
+#type3
+id=1003;
+name="Vignarathan"
+gr="10A"
+ic=200412121313
+
+output=f"Student ID \t: {id}\nStudent Name \t: {name}\nGrade Name \t: {gr}\nNIC No \t\t: {ic}"
+print("-----------------------------")
+print("Student Information")
+print("-----------------------------")
+print(output)
+print("-----------------------------")
