62a286365bd03676d88389d915f7222e357824bd
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:56:32 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,19 @@
+x=int(input("Enter Your Marks : "))
+if(100>=x>=0):
+    if(x>74):
+        print("Result : A")
+    elif(x>64):
+        print("Result : B")
+    elif(x>54):
+        print("Result : C")
+    elif(x>44):
+        print("Result : S")
+    else:
+        print("Fail")
+else:
+    print("Invalid Marks")
+    
+    
+    
+    
+    
