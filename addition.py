7b1fa1c8ebda5a0cd724d42384e5f484e6a7dedc
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:55:20 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,10 @@
+x=int(input("Enter First Number : "))
+y=int(input("Enter Second Number : "))
+z=x+y
+print("Total :",z)
+
+print("***************")
+a=input("Enter Third Number : ")
+b=input("Enter Fourth Number : ")
+c=int(a)+int(b)
+print("Total :",c)
