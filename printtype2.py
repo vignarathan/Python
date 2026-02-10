0c6133a7363a924d01f32954079519673711507b
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:57:01 +0530

    Create printtype2.py

---
+++ printtype2.py
@@ -0,0 +1,8 @@
+#type2
+id=1003;
+name="Vignarathan"
+gr="10A"
+ic=200412121313
+
+print("-----------------------------","\n\tStudent Information","\n-----------------------------","\nStudent ID","\t:",id,"\nStudent Name","\t:",name,"\nGrade Name","\t:",gr,"\nNIC No","\t\t:",ic,"\n-----------------------------")
+
