aa55a05053d9242529030af0b19a330456689f0e
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:57:14 +0530

    Create printtype5.py

---
+++ printtype5.py
@@ -0,0 +1,12 @@
+#type5
+id=1003;
+name="Vignarathan"
+gr="10A"
+ic=200412121313
+
+
+print("-----------------------------")
+print("Student Information")
+print("-----------------------------")
+print("Student ID \t: %d\nStudent Name \t: %s\nGrade Name \t: %s\nNIC No \t\t: %d"%(id,name,gr,ic))
+print("-----------------------------")
