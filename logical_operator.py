d981803ddc26f7c3e5c0829571f482f4d548ef0e
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:56:43 +0530

    Create logical_operator.py

---
+++ logical_operator.py
@@ -0,0 +1,13 @@
+x=True
+y=False
+
+print(x and y)#False
+print(x or y)#True
+print(not x)#False
+
+x=10
+y=20
+                               
+print(x>y and x<30)#False
+print(x>y or y>5)#True
+print(not(x==y))#True
