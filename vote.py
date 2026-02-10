4d46ad7b50bde4653b3426b54281c3716f02d683
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:57:44 +0530

    Create vote.py

---
+++ vote.py
@@ -0,0 +1,8 @@
+x=int(input("Enter Your Age :"))
+if(150>=x>=0):
+    if(x>=18):
+        print("Eligible for Vote")
+    else:
+        print("Not eligible for Vote")
+else:
+    print("Invalid Age")
