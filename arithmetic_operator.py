408a5ce5661e11d9ee8d8c8ce7c97b5a9d8bcd18
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:55:59 +0530

    Create arithmetic_operator.py

---
+++ arithmetic_operator.py
@@ -0,0 +1,13 @@
+x=int(input("Enter Number1 : "))
+y=int(input("Enter Number2 : "))
+
+print("Addition(+) \t\t:",(x+y))
+print("Subtraction(-) \t\t:",(x-y))
+print("Multiplication(*) \t:",(x*y))
+print("Division(/) \t\t:",(x/y))
+print("Modulus(%) \t\t:",(x%y))
+print("Exponentiation(**) \t:",(x**y))
+print("Floor Division(//) \t:",(x//y))
+
+
+
