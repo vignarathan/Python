2d75797295084e8b9bc5bca5de24c9001f59a23a
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:56:27 +0530

    Create fullname.py

---
+++ fullname.py
@@ -0,0 +1,12 @@
+fname="Vignarathan"
+lname="Sivalingam"
+em="s.vikki@2026@gmail.com"
+bpay=150000
+print("**********************************************")
+print("Employee Details")
+print("**********************************************")
+print("Full Name","\t:",fname,lname)
+print("Email","\t\t:",em)
+print("Basic Salary","\t:","Rs."+str(bpay))
+print("**********************************************")
+
