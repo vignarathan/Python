2dd11ca20e7872bd14191e7998ee9386e13909b0
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-10 20:56:56 +0530

    Create printtype1.py

---
+++ printtype1.py
@@ -0,0 +1,14 @@
+#type1
+id=1003;
+name="Vignarathan"
+gr="10A"
+ic=200412121313
+
+print("-----------------------------")
+print("\tStudent Information")
+print("-----------------------------")
+print("Student ID","\t:",id)
+print("Student Name","\t:",name)
+print("Grade Name","\t:",gr)
+print("NIC No","\t\t:",ic)
+print("-----------------------------")
