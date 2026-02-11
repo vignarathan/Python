d607ccef0e700aa29c4bc12acad3d11104499e4f
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-11 14:54:06 +0530

    Create nested_evenodd.py

---
+++ nested_evenodd.py
@@ -0,0 +1,8 @@
+num=int(input("Enter the Number : "))
+if(num>0):
+    if((num%2)==0):
+        print(num,"is a even number")
+    else:
+        print(num,"is a odd number")
+else:
+    print("It's a negative value")
