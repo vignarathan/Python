f292f815e734d14ac92155439eaf6c1d244b9a67
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-12 10:01:47 +0530

    Create day_match.py

---
+++ day_match.py
@@ -0,0 +1,20 @@
+day=int(input("Enter the Number(1to7) : "))
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid Input")
+        
