ca8e9ad6fd716ddf1d12c8647a58b52e74296d3f
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-16 12:12:47 +0530

    Add loop examples and odd/even scripts
    Add several small demo scripts: loop_print_sidebyside.py, loop_print_sidebyside2.py, and loop_print_sidebyside3.py (print numbers 1..6 with different separators), odd.py (prints odd numbers 1..9), and odd_and_even.py (classifies numbers 1..10 as odd or even). Also clean up whilename.py by removing an extraneous semicolon and normalizing formatting.

---
+++ odd_and_even.py
@@ -0,0 +1,7 @@
+num=1
+while num<=10:
+    if (num%2==0):
+        print(num,"is a even number")
+    else:
+        print(num,"is a odd number")
+    num+=1
--- whilename.py
+++ whilename.py
@@ -1,4 +1,4 @@
-x=1;
+x=1
 while x<=10:
  print("S.Vignarathan")
  x+=1
