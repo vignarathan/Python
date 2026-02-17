e4944fff9f01d88ead599339461572da245a0124
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-17 11:04:59 +0530

    Create forloop2.py

---
+++ forloop2.py
@@ -0,0 +1,12 @@
+for x in range (1,10,2):
+    print(x)
+print("")
+
+for y in range(10,0,-1):
+    print(y)
+    
+
+"""for i in True:
+    print(i)
+    
+  it can not use here, here range used not condition"""
