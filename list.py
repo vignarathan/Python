1eec7a42189bc7ebd30eff76e9b7284960cea304
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-23 09:54:10 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+subjects=["Maths","Science","Tamil","ICT","English"]
+print(subjects)
+print(type(subjects))
+print(subjects[0])
+print(subjects[-1])
+print(len(subjects))
+subjects[0]="History"
+print(subjects)
+subjects.append("Physics")
+print(subjects)
