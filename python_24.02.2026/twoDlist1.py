8c4f0e38cd0ccea6e8fa0d80050522597a70b7a7
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-24 10:25:53 +0530

    Create twoDlist1.py

---
+++ python_24.02.2026/twoDlist1.py
@@ -0,0 +1,20 @@
+x=[
+    [10,20,30],
+    [40,50,60],
+    [70,80,90]
+   ]
+print(x)
+print(type(x))
+
+print(x[0][0])
+print(x[0][1])   
+print(x[0][2])   
+print(x[1][0])   
+print(x[1][1])   
+print(x[1][2])   
+print(x[2][0])   
+print(x[2][1])   
+print(x[2][2])   
+   
+   
+   
