58ecd7c26db49e26b1730ecaf7b286046e5037f2
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-24 10:30:06 +0530

    Update dictionary2.py

--- python_24.02.2026/dictionary2.py
+++ python_24.02.2026/dictionary2.py
@@ -1,11 +1,15 @@
 d=[("name","vignarathan"),("age",21),("gender","male")]
 print(type(d))
+
 data=dict(d)
 print(data)
 print(type(data))
+
 print(data["name"])
 print(data["age"])
 print(data["gender"])
+#print(data["address"])   keyerror:address
+
 print(data.get("name"))
-print(data.get("city"))  #None
+print(data.get("city"))  #there is no error, output:None
 
