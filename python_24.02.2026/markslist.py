35013cd30c0d81ff8e06b8b80bf8afac33963f23
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-25 10:40:00 +0530

    Update markslist.py

--- python_24.02.2026/markslist.py
+++ python_24.02.2026/markslist.py
@@ -4,9 +4,9 @@ marks=[[95,86,75],[93,87,69],[83,72,56],[72,45,63],[97,87,78]]
 total=[]
 average=[]
 result=[]
-for x in range(5):
+for x in range(len(student)):
     tot=ave=0
-    for y in range(3):
+    for y in range(len(subject)):
         tot+=marks[x][y]
     total.append(tot)
     
@@ -23,9 +23,9 @@ for x in range(5):
         re="False"
     result.append(re)
 
-print(f"{'StudentName':<15}{'Maths':<8}{'Science':<10}{'English':<10}{'Total':<8}{'Average':<10}{'Result':<9}")
+print(f"{'StudentName':<15}{'Maths':<8}{'Science':<10}{'English':<10}{'Total':<8}{'Average':<12}{'Result':<9}")
 for x in range(5):
     print(f"{student[x]:<15}",end="")
     for y in range(3):
         print(f"{marks[x][y]:<10}",end="")
-    print(f"{total[x]:<8}{average[x]:<10,.2f}{result[x]:<7}")
+    print(f"{total[x]:<8}{average[x]:<10,.2f}{result[x]:<5}")
