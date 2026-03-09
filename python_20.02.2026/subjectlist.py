56561b0d246e392b0fd5f751401666938faae585
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-09 09:55:37 +0530

    Create subjectlist.py

---
+++ python_20.02.2026/subjectlist.py
@@ -0,0 +1,9 @@
+subjects=["Maths","Science","Tamil","ICT","English","History"]
+for x in range(len(subjects)):
+    print(subjects[x])
+    
+print("*****************")
+x=0    
+while x<len(subjects):
+    print(subjects[x])
+    x+=1
