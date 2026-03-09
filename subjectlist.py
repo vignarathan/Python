8998f7bcbadb85de0cd1deafe4bff5fd1787a9b3
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-09 09:55:41 +0530

    Delete subjectlist.py

--- subjectlist.py
+++
@@ -1,9 +0,0 @@
-subjects=["Maths","Science","Tamil","ICT","English","History"]
-for x in range(len(subjects)):
-    print(subjects[x])
-    
-print("*****************")
-x=0    
-while x<len(subjects):
-    print(subjects[x])
-    x+=1
